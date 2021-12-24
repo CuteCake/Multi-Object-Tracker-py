d077ace60fef0fa432498fdf9fb03c3558b11817
Zhihao <zhdeng@berkeley.edu>
2021-12-23 18:24:04 -0800

    Updated Env

--- enviroment.py
+++ enviroment.py
@@ -16,19 +16,21 @@ class Point:
         self.pointID = pointID
         self.angular_velocity = angular_velocity
 
-    def update(self,dt):
+
+    def update(self,dt): #This is a constant velocity, constant turning rate model, this update is for ground truth generation
         self.posx += self.velocity*dt*math.cos(self.twist)
         self.posy += self.velocity*dt*math.sin(self.twist)
-        self.angular_velocity = random.gauss(0,0.1)
+        self.angular_velocity = random.gauss(0,0.5)
         self.twist += self.angular_velocity*dt
         self.twist = self.twist%(2*math.pi)
+        self.velocity += random.gauss(0,2)
         
 
     def getXY(self):
         return (self.posx,self.posy)
         
 class PointsEnv:
-    def __init__(self, width, height, numPoints, pointSize = 2, observation_noise = 0.1):
+    def __init__(self, width, height, numPoints, pointSize = 2, observation_noise = 5):
         self.width = width
         self.height = height
         self.numPoints = numPoints
@@ -41,6 +43,9 @@ class PointsEnv:
         self.boxSizeY = height / 20
         self.observation_noise = observation_noise
 
+        self.clock = pygame.time.Clock()
+
+
     def generatePoints(self):
         for i in range(self.numPoints):
             self.points.append(self.generatePoint(i))
@@ -49,8 +54,8 @@ class PointsEnv:
         point_a = Point(random.randrange(0,self.width), # x
             random.randrange(0,self.height), # y
             random.uniform(0,2*math.pi), # twist
-            (random.uniform(0,1)+1), # velocity
-            0, # angular_velocity
+            (random.uniform(0,4)+20), # initial velocity
+            0 + random.gauss(0,10), # initial angular_velocity
             id)
         return point_a
 
@@ -67,30 +72,40 @@ class PointsEnv:
 
             box = box.astype(np.int32)
             #rotate the box using numpy, origin is the center of the box
-            box_relative = np.dot(box - box.mean(axis=0), np.array([[math.cos(point.twist), -math.sin(point.twist)],
-                                                            [math.sin(point.twist), math.cos(point.twist)]]))
-            box = box_relative + box.mean(axis=0)
+            box_relative = np.dot(np.array([[math.cos(point.twist), -math.sin(point.twist)],
+                                                            [math.sin(point.twist), math.cos(point.twist)]]),
+                                                            (box - box.mean(axis=0)).T )
+            box = box_relative.T + box.mean(axis=0)
             pygame.draw.polygon(screen, (100, 255, 100), box, width=3)
 
 
 
-    def update(self,time):
+    def update(self):
+        dt = self.get_last_dt()
         for i in range(self.numPoints):
-            self.points[i].update(time)
+            self.points[i].update(dt)
         
         #check if the point is inside the frame
         for point in self.points:
             if point.posx < 0 or point.posx > self.width or point.posy < 0 or point.posy > self.height :
                 pointID = point.pointID
                 self.points[pointID] = self.generatePoint(pointID)
+        
+        self.clock.tick(60) #This limits The env to 60 frames per second by adding delay to the loop
 
-    def getObservation(self):
         #return the observation as a 1D array
         observation = np.empty((self.numPoints,2))
         for i in range(len(self.points)):
             observation[i,0] = (self.points[i].posx + random.gauss(0,self.observation_noise))
             observation[i,1] = (self.points[i].posy + random.gauss(0,self.observation_noise))
         return observation
+
+    def draw_observed_points(self,screen,obsList):
+        for i in range(len(obsList)):
+            pygame.draw.circle(screen, (255, 10, 105), (int(obsList[i][0]),int(obsList[i][1])), self.pointSize)
+
+    def get_last_dt(self):
+        return self.clock.get_time()/1000.0
 
 if __name__ == "__main__":
     pygame.init()
@@ -99,10 +114,12 @@ if __name__ == "__main__":
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print('time elapsed: ', env.get_time_elapsed())
                 pygame.quit()
                 quit()
         screen.fill((0, 0, 0))
         env.draw(screen)
-        env.update(0.03)
-        print(env.getObservation())
+        observation = env.update()
+        env.draw_observed_points(screen, observation)
+        # print(env.getObservation())
         pygame.display.update()
